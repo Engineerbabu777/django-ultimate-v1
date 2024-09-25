b2791ce0c0b9b3e0b14dc265de64631d0fff2901
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-09-25 19:08:07 +0500

    add: login view with form custom one, logout view, etc

--- learning/base/views.py
+++ learning/base/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render,redirect
-from .models import Room
+from .models import Room,Topic,User
 from .forms import RoomForm
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 # rooms = [
 #     {'id':1, 'name':'Room 1'},
@@ -11,6 +16,37 @@ from .forms import RoomForm
     
 # ]
 # Create your views here.
+
+def loginPage(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'POST':
+        email = request.POST.get('email').lower()
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(email=email)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username OR password does not exit')
+
+    context = {'page': page}
+    return render(request, 'base/login_register.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
 def home(request):
     return render(request, 'base/home.html')
 
@@ -19,9 +55,9 @@ def room_page(request,pk):
     
    
     room = Room.objects.get(id=pk)
-    
+    topics = Topic.objects.all()
   
-    return render(request, 'base/room.html',{'room':room})
+    return render(request, 'base/room.html',{'room':room,'topics':topics})
 
 
 
